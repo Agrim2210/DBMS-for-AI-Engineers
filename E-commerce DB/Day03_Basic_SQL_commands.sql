5b41060aba9a68e0f6fe96b6e4152893eb72dd42
Agrim Rao <agrimrana2005@gmail.com>
2026-03-14 15:45:58 +0530

    Implemented Basic SQL Queries(WHERE,ORDER BY,LIMIT)

---
+++ E-commerce DB/Day03_Basic_SQL_commands.sql
@@ -0,0 +1,8 @@
+use e_commerce;
+-- used WHERE to filter data
+SELECT * FROM customers WHERE city= "Delhi";
+SELECT * from products WHERE price>10000;
+
+SELECT * from payment WHERE payment_status ="Completed";
+
+-- ORDER BY
